053fba6f713e82cc43363c0e5d2815b01c0b908f
Kaveh Nejad <kaveh2009@hhotmail.co.uk>
2020-11-26 13:48:07 +0000

    can toggle visited in marker popup

--- prototype/js/MapPage/filmingLocationsClass.js
+++ prototype/js/MapPage/filmingLocationsClass.js
@@ -3,12 +3,13 @@ class FilmingLocation{
         this.movie = movie;
         this.location = openCageAPIConvertToLatLong(locationByName, this.setMarker, this);
         this.name = locationByName;
+        this.visited = false;
         movie.filmingLocationsMarkers.push(this);
     }
     
     setMarker(filmingLocation, coordinates){
         filmingLocation.movie.setUpFilmingLocationByNameAndLatLong(filmingLocation.name, coordinates);
         filmingLocation.location = coordinates;        
-        filmingLocation.marker = new Marker(filmingLocation.location, filmingLocation.name, filmingLocation.movie)
+        filmingLocation.marker = new Marker(filmingLocation.location, filmingLocation.name, filmingLocation.movie, filmingLocation);
     }
 }
--- prototype/js/MapPage/markerClass.js
+++ prototype/js/MapPage/markerClass.js
@@ -1,22 +1,49 @@
+var numberOfMarkers = 0;
+var  buttonMap = {};
+
 class Marker{
-    constructor(position, label, movie){
+    constructor(position, label, movie, parent){
+        this.markerID = makeMarkerID();
+        buttonMap[this.markerID] = this;
         this.position = position;
         this.label = label;
         this.movie = movie;
+        this.parent = parent;
         this.createMarker();
         this.addToMap();
     }
-
+    
     createMarker(){
         this.marker = L.marker(this.position);
-        this.marker.bindPopup(this.movie.name + "  " + this.movie.year + "<br><br>" + this.label);
+        this.creatMarkerPopUp();
     }
-
+    
+    creatMarkerPopUp(){
+        var toggleVisitButton = "<button onclick=markerToggleVisistedButtonClicked(" + this.markerID + ")>toggle visited</button>";
+        var movieNameAndYear = this.movie.name + "  " + this.movie.year;
+        var visited = "Visited:" + this.parent.visited;
+        
+        this.marker.bindPopup(movieNameAndYear + "<br><br>" + this.label +  "<br><br>" + visited + toggleVisitButton);
+    }
+    
+    
     addToMap(){
         this.marker.addTo(mymap);
     }
-
+    
     removeFromMap(){
         this.marker.removeFrom(mymap);
     }
+    
+}
+
+function makeMarkerID(){
+    return ++numberOfMarkers;
+}
+
+function markerToggleVisistedButtonClicked(markerID){
+    var marker = buttonMap[markerID];
+    marker.parent.visited = !marker.parent.visited;
+    marker.creatMarkerPopUp();
+
 }
