738ab77f3a5150ff4ac51c1cda140a9be9a13877
Kaveh Nejad <kaveh2009@hhotmail.co.uk>
2020-12-04 10:18:03 +0000

    created directions branch

--- prototype/js/DirectionsPage/directions.js
+++ prototype/js/DirectionsPage/directions.js
@@ -1,7 +1,19 @@
 var currentPossition;
+const BASEGOOGLEDIRECTIONSURL = "https://maps.googleapis.com/maps/api/directions/json?";
 
 navigator.geolocation.getCurrentPosition(setCurrentPosition);
 
 function setCurrentPosition(possition){
     currentPossition = [possition.coords.latitude, possition.coords.longitude];
+}
+
+function getDirectioons(from, to){
+    var url =  BASEGOOGLEDIRECTIONSURL + "origin=" + from + "destination=" + to;
+    console.log(url)
+    $.getJSON(url, onDirectionsCalculated)
+}
+
+
+function onDirectionsCalculated(something){
+    console.log(something)
 }
