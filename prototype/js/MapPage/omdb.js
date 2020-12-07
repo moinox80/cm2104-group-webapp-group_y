74d719edaf64e953859d87bb319ebb9684a5b9e4
Arthur Sidle <56030962+ArthurSidle@users.noreply.github.com>
2020-12-07 11:26:52 +0000

    omdb.js functions now take callbacks Refactored map.js to accommodate

--- prototype/js/MapPage/omdb.js
+++ prototype/js/MapPage/omdb.js
@@ -5,22 +5,22 @@
 
 var baseURL = "http://www.omdbapi.com/?apikey=7c3f362c&";//base url without movie id or name, kavehs api key
 
-function getResultsFromOMDBByName(movieName){
+function getResultsFromOMDBByName(movieName, callback){
     var url = baseURL + "s=" + movieName;
     
     $.getJSON(url, function(jsondata){
         if(jsondata.Search){
-            selectMovieFromOptions(jsondata.Search)
+            callback(jsondata.Search)
         };
     });
 };
 
-function getResultsFromOMDBByID(movieID){
+function getResultsFromOMDBByID(movieID, callback){
     var url = baseURL + "i=" + movieID;
     
     $.getJSON(url, function(jsondata){
         if(jsondata){
-            makeMovie(jsondata);
+            callback(jsondata);
         };
     });
 };
