34ead600903ecc583c5fe85205cba79d54e822a6
Arthur Sidle <56030962+ArthurSidle@users.noreply.github.com>
2020-12-07 18:00:57 +0000

    Implemented main functionality for mystalks.html

--- prototype/js/MyStalksPage/myStalksPage.js
+++ prototype/js/MyStalksPage/myStalksPage.js
@@ -1,33 +1,173 @@
-function addLocationsToCard($card, json) {
-    for (var i = 0; i < json.locations.length; i++) {
-        addLocationToList($card.find(".card-body").find(".collapse"), json.locations[i].location);
+class Card {
+    constructor(movieID) {
+        this.imdbID = movieID;
+        this.movieName;
+
+        this.currentLocation = false;
+        this.filmingLocations = [];
+        this.distances = [];
+        this.filmingLocationsJSON;
+
+        this.$card;
+        this.$locationList = [];
+        this.addCardTemplate();
+
+        getResultsFromOMDBByID(this.imdbID, function(movie) {
+            this.movieName = movie.Title;
+
+            this.$card.find(".movie-title").text(movie.Title);
+            this.$card.find(".movie-facts").eq(0).html(`
+                <li>Release year: ${movie.Year}</li>
+                <li>Genres: ${movie.Genre}</li>
+                <li>Rating: ${movie.Rated}</li>
+            `);
+            this.$card.find(".movie-facts").eq(1).html(`
+                <li>Director: ${movie.Director}</li>
+                <li>Writers: ${movie.Writer}</li>
+                <li>Actors: ${movie.Actors}</li>
+            `);
+
+            this.addLocationsToCard();
+        }.bind(this));
     }
-}
 
-function addLocationToList($list, location, proximity) {
-    var appendString =
-    `<div class="row">
-        <div class="col-lg-8">
-            <label class="form-check-label">
-                <input type="checkbox" class="form-check-input" value="">${location}
-            </label>
-        </div>
-        <div class="col-lg-2">
-            Proximity: ${proximity} miles
-        </div>
-        <div class="col-lg-2">
-            See on map >
-        </div>
-    </div>`;
+    addCardTemplate() {
+        var appendString =
+        `<div class="card m-2 p-2 shadow-sm movie-box">
+            <div class="card-header">
+            <div class="row movie-info">
+                <div class="col-lg-4">
+                <h2 class="movie-title"></h2>
+                </div>
+                <div class="col-lg-4">
+                <ul class="movie-facts">
+                    <li>Release year: </li>
+                    <li>Genres: </li>
+                    <li>Rating: </li>
+                </ul>
+                </div>
+                <div class="col-lg-4">
+                <ul class="movie-facts">
+                    <li>Director: </li>
+                    <li>Writer: </li>
+                    <li>Actors: </li>
+                </ul>
+                </div>
+            </div>
+            </div>
 
-    $list.append(appendString);
+            <div class="card-body">
+                <button data-toggle="collapse" data-target="#demo">
+                    <h5>Locations</h5>
+                </button>
+                <div id="demo" class="collapse">
+                    
+                </div> 
+            </div>
+        </div>`
+        
+
+        this.$card = $(appendString).appendTo(".container");
+    }
+
+    addLocationsToCard() {
+        this.currentLocation = navigator.geolocation.getCurrentPosition(function(position) {
+            this.currentLocation = [position.coords.longitude, position.coords.latitude];
+        }.bind(this));
+
+        var apiKey = "a9eb244cd4msh1023a1ad25868ebp174b04jsn1fa8197ee780";
+        var url = "https://imdb8.p.rapidapi.com/title/get-filming-locations?tconst=" + this.imdbID + "&rapidapi-key=" + apiKey;
+
+        $.getJSON(url, function(data) {
+            this.filmingLocationsJSON = data;
+            this.filmingLocationsJSON.length = this.filmingLocationsJSON.locations.length;
+            for (var i = 0; i < this.filmingLocationsJSON.locations.length; i++) {
+                this.addLocationTemplate();
+
+                var locationText = this.filmingLocationsJSON.locations[i].location;
+                this.$locationList[i].find(".form-check-label").append(locationText);
+
+                /*
+                    While the following function was originally intended to take a FilmingLocation object as its third parameter,
+                    it just so happens it has the right number of arguments for the purpose of ordering the filming locations in the array
+                */
+
+
+                openCageAPIConvertToLatLong(locationText, function(index, position){
+                    this.filmingLocations[index] = position;
+                    this.waitForCurrentLocation(index, position);
+                }.bind(this), i);
+            }
+        }.bind(this));
+    }
+
+    addLocationTemplate() {
+        this.$locationList.push($(`
+            <div class="row">
+                <div class="col-lg-8">
+                    <label class="form-check-label">
+                        <input type="checkbox" class="form-check-input" value="">
+                    </label>
+                </div>
+                <div class="col-lg-2 proximity">
+                    
+                </div>
+                <div class="col-lg-2">
+                    See on map >
+                </div>
+            </div>
+        `).appendTo(this.$card.find(".collapse")));
+    }
+
+    waitForCurrentLocation(index, position) {
+        if (this.currentLocation === false) {
+            setTimeout(function(){ this.waitForCurrentLocation(index, position) }.bind(this), 500)
+            return;
+        } else {
+            this.distances[index] = this.getDistance(this.filmingLocations[index]);
+            this.$card.find(".proximity").text(this.distances[index] + " miles");
+        }
+    }
+
+    getDistance(filmingLocation) {
+        const lon1 = this.currentLocation[0];
+        const lat1 = this.currentLocation[1];
+        const lon2 = filmingLocation[0];
+        const lat2 = filmingLocation[1];
+
+        //Code from https://www.movable-type.co.uk/scripts/latlong.html
+        const R = 6371e3; // metres
+        const φ1 = lat1 * Math.PI/180; // φ, λ in radians
+        const φ2 = lat2 * Math.PI/180;
+        const Δφ = (lat2-lat1) * Math.PI/180;
+        const Δλ = (lon2-lon1) * Math.PI/180;
+
+        const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+                Math.cos(φ1) * Math.cos(φ2) *
+                Math.sin(Δλ/2) * Math.sin(Δλ/2);
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+        const d = R * c; // in metres
+        
+        var miles = this.metresToMiles(d);
+        return Math.round(miles * 100) / 100;
+
+        /*var xDistance = filmingLocation[0] - this.currentLocation[0];
+        var yDistance = filmingLocation[1] - this.currentLocation[1];
+        var fullDistance = Math.sqrt(Math.pow(xDistance,2) + Math.pow(yDistance,2));
+        return fullDistance;*/
+    }
+
+    metresToMiles(metres) {
+        return metres / 1609.344;
+    }
+
 }
 
 $(function(){
-    /*var url = "https://imdb8.p.rapidapi.com/title/get-filming-locations?tconst=tt0087332&rapidapi-key=a9eb244cd4msh1023a1ad25868ebp174b04jsn1fa8197ee780";
-    $.getJSON(url, function(data) {
-        addLocationsToCard(data, $(".card"));
-    });*/
-
-    addLocationsToCard($(".card"), {"@type":"imdb.api.title.filminglocations","base":{"id":"/title/tt0087332/","image":{"height":1426,"id":"/title/tt0087332/images/rm1280169216","url":"https://m.media-amazon.com/images/M/MV5BMTkxMjYyNzgwMl5BMl5BanBnXkFtZTgwMTE3MjYyMTE@._V1_.jpg","width":928},"runningTimeInMinutes":105,"title":"Ghostbusters","titleType":"movie","year":1984},"id":"/title/tt0087332/","locations":[{"extras":["interiors: Ghostbusters headquarters"],"id":"/title/tt0087332/filminglocations/lc0127277","interestingVotes":{"up":27},"location":"Fire Station 23 - 225 E. 5th Street, Los Angeles, California, USA"},{"attributes":["exterior"],"extras":["Ghostbusters headquarters exteriors"],"id":"/title/tt0087332/filminglocations/lc0127278","interestingVotes":{"down":1,"up":21},"location":"Hook & Ladder Company #8 - 14 North Moore Street, Manhattan, New York City, New York, USA"},{"extras":["the restaurant where Louis tries to escape a ravening hellhound"],"id":"/title/tt0087332/filminglocations/lc0127285","interestingVotes":{"up":13},"location":"Tavern on the Green - Central Park at W. 67th Street, Central Park, Manhattan, New York City, New York, USA"},{"extras":["Sedgewick Hotel"],"id":"/title/tt0087332/filminglocations/lc0127275","interestingVotes":{"up":11},"location":"Biltmore Hotel - 506 S. Grand Avenue, Downtown, Los Angeles, California, USA"},{"extras":["Spook Central"],"id":"/title/tt0087332/filminglocations/lc0127274","interestingVotes":{"up":10},"location":"55 Central Park West, Manhattan, New York City, New York, USA"},{"attributes":["exterior"],"extras":["Library exterior and main floor"],"id":"/title/tt0087332/filminglocations/lc0127280","interestingVotes":{"up":10},"location":"New York Public Library - Fifth Avenue & 42nd Street, Manhattan, New York City, New York, USA"},{"extras":["Department of Parapsychology/Weaver Hall"],"id":"/title/tt0087332/filminglocations/lc0127276","interestingVotes":{"up":9},"location":"Columbia University - Broadway & 116th Street, Manhattan, New York City, New York, USA"},{"extras":["interiors: Library basement"],"id":"/title/tt0087332/filminglocations/lc0127279","interestingVotes":{"up":9},"location":"Los Angeles Central Library - 630 W. Fifth Street, Downtown, Los Angeles, California, USA"},{"extras":["Manhattan City Bank"],"id":"/title/tt0087332/filminglocations/lc0127273","interestingVotes":{"up":7},"location":"489 Fifth Avenue, Manhattan, New York City, New York, USA"},{"attributes":["studio"],"id":"/title/tt0087332/filminglocations/lc0127281","interestingVotes":{"up":5},"location":"Stage 12, Warner Brothers Burbank Studios - 4000 Warner Boulevard, Burbank, California, USA"},{"attributes":["studio"],"id":"/title/tt0087332/filminglocations/lc0127282","interestingVotes":{"up":4},"location":"Stage 15, Warner Brothers Burbank Studios - 4000 Warner Boulevard, Burbank, California, USA"},{"attributes":["studio"],"id":"/title/tt0087332/filminglocations/lc0127283","interestingVotes":{"up":4},"location":"Stage 16, Warner Brothers Burbank Studios - 4000 Warner Boulevard, Burbank, California, USA"},{"attributes":["studio"],"id":"/title/tt0087332/filminglocations/lc0127284","interestingVotes":{"up":4},"location":"Stage 18, Warner Brothers Burbank Studios - 4000 Warner Boulevard, Burbank, California, USA"},{"extras":["Town House"],"id":"/title/tt0087332/filminglocations/lc1504361","interestingVotes":{"up":3},"location":"420 East 78th Street, New York, USA"},{"extras":["location"],"id":"/title/tt0087332/filminglocations/lc1454958","interestingVotes":{"up":2},"location":"Lincoln Center, New York City, New York, USA"},{"id":"/title/tt0087332/filminglocations/lc1504362","interestingVotes":{"up":1},"location":"U.S. Customs House, New York City, New York, USA"}]});
+    /*
+    Note: This script can only display a select set of movies.
+    The final product would store movies of interest for a user in a backend database.
+    */
+    new Card("tt0087332");
 });
