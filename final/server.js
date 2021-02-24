9a41a67fa7191b00b513a0e271d2f4ca9dec1c0e
Kaveh Nejad <kaveh2009@hotmail.co.uk>
2021-02-24 12:04:38 +0000

    put all pages on server js

--- final/server.js
+++ final/server.js
@@ -14,4 +14,40 @@ app.get('/about', (req, res) => {
   res.render("pages/about");
 });
 
+app.get('/account', (req, res) => {
+  res.render("pages/account");
+});
+
+app.get('/contact', (req, res) => {
+  res.render("pages/contact");
+});
+
+app.get('/directions', (req, res) => {
+  res.render("pages/directions");
+});
+
+app.get('/directions', (req, res) => {
+  res.render("pages/directions");
+});
+
+app.get('/login', (req, res) => {
+  res.render("pages/login");
+});
+
+app.get('/map', (req, res) => {
+  res.render("pages/map");
+});
+
+app.get('/movie', (req, res) => {
+  res.render("pages/movie");
+});
+
+app.get('/mystalks', (req, res) => {
+  res.render("pages/mystalks");
+});
+
+app.get('/signup', (req, res) => {
+  res.render("pages/signup");
+});
+
 app.listen(8080);
