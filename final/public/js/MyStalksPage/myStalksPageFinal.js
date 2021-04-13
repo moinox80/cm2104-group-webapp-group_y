dd96bf735cad86dbe5c3694f7a7ed857080aecc1
Arthur Sidle <1911859@rgu.ac.uk>
2021-04-13 11:31:31 +0000

    Debug of mystalks page location checklist functionality

--- final/public/js/MyStalksPage/myStalksPageFinal.js
+++ final/public/js/MyStalksPage/myStalksPageFinal.js
@@ -1,20 +1,24 @@
-const e = require("express");
+//const e = require("express");
 
-$('[type=checkbox]').change(function() {
-  // $(this) should hopefully refer to the current checkbox
-  let imdbID = $(this).attr('id');
-  let locationByName = $(this).parent().text();
-  if ($(this).is(':checked')) {
-    sendMovieIdToServerRemoveFromMyStalks(imdbID, locationByName);
-  } else {
-    sendMovieIdToServerAddToMyStalks(imdbID, locationByName);
-  }
+$(function() {
+  $('[type=checkbox]').change(function() {
+    // $(this) should hopefully refer to the current checkbox
+    let imdbID = $(this).attr('class').split(' ')[1];
+    let locationByName = $(this).parent().find('.locationText').text();
+    console.log("A checkbox of movie " + imdbID + " and location " + locationByName + " was clicked");
+    if ($(this).is(':checked')) {
+      sendMovieIdToServerAddToMyStalks(imdbID, locationByName);
+    } else {
+      sendMovieIdToServerRemoveFromMyStalks(imdbID, locationByName);
+    }
+  });
 });
+
 
 function sendMovieIdToServerAddToMyStalks(imdbID, locationByName) {
   //https://stackoverflow.com/questions/59511205/how-to-send-string-from-client-to-server-via-post
   var url = '/addLocationToVisited';
-  var body = 'imdbID' + imdbID + '&locationByName=' + locationByName;
+  var body = 'imdbID=' + encodeURIComponent(imdbID) + '&locationByName=' + encodeURIComponent(locationByName);
   var xhr = new XMLHttpRequest();
   xhr.open('post', url, true);
   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
@@ -24,7 +28,7 @@ function sendMovieIdToServerAddToMyStalks(imdbID, locationByName) {
 function sendMovieIdToServerRemoveFromMyStalks(imdbID, locationByName) {
   //https://stackoverflow.com/questions/59511205/how-to-send-string-from-client-to-server-via-post
   var url = '/removeLocationFromVisited';
-  var body = 'imdbID' + imdbID + '&locationByName=' + locationByName;
+  var body = 'imdbID=' + imdbID + '&locationByName=' + locationByName;
   var xhr = new XMLHttpRequest();
   xhr.open('post', url, true);
   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
